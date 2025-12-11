5479e7392f0501dc156590e01742ba2ba343ecfb
lovie stc <lovie.stc26@gmail.com>
2025-12-11 06:23:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4711969509-2cbf5",
+  "appId": "1:546775789966:web:06656931ccbc7172eaf4df",
+  "apiKey": "AIzaSyBmZswB-1MUYBW6nOTp3Qq6j0VBuk9eX-I",
+  "authDomain": "studio-4711969509-2cbf5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "546775789966"
+};
